d76972c61b8420b7a91fb39fc976215b11afc9b1
Nara Wang <nara.wang04@gmail.com>
2024-06-12 15:32:35 -0400

    add connection

--- src/main/java/com/algonquin/cst8288/assignment2/database/DBConnection.java
+++ src/main/java/com/algonquin/cst8288/assignment2/database/DBConnection.java
@@ -5,16 +5,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	
+	private static DBConnection instance = null;
 	private static Connection connection = null;
 
 
 	private String serverUrl = "jdbc:mysql://localhost:3306/bookvault";
 	private String userString = "root";
-	private String passwordString = "password";
+	private String passwordString = "vistopia";
 	private String driverString = "com.mysql.cj.jdbc.Driver";
 
-	private DBConnection(String serverUrl, String userString, String passwordString, String driverString) {
+	private DBConnection() throws SQLException {
+		try {
+		Class.forName(driverString);
+		this.connection = DriverManager.getConnection(serverUrl, userString, passwordString);
+	} catch (ClassNotFoundException ex) {
+		System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+		}
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public static DBConnection getInstance() throws SQLException {
+		if (instance == null) {
+			instance = new DBConnection();
+		} else if (instance.getConnection().isClosed()) {
+			instance = new DBConnection();
+		}
+		return instance;
+	}
 
 	}
-}
