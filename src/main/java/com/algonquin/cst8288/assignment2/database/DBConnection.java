1bd8b35d51f8675fdb2cd561ec2e33d1f148b867
Nara Wang <nara.wang04@gmail.com>
2024-06-12 14:54:13 -0400

    add database part/not finished yet

--- src/main/java/com/algonquin/cst8288/assignment2/database/DBConnection.java
+++ src/main/java/com/algonquin/cst8288/assignment2/database/DBConnection.java
@@ -1,14 +1,20 @@
 package com.algonquin.cst8288.assignment2.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
 	
 	private static Connection connection = null;
-	
+
+
 	private String serverUrl = "jdbc:mysql://localhost:3306/bookvault";
-	private String userString = "***";
-	private String passwordString = "***";
+	private String userString = "root";
+	private String passwordString = "password";
 	private String driverString = "com.mysql.cj.jdbc.Driver";
-	
+
+	private DBConnection(String serverUrl, String userString, String passwordString, String driverString) {
+
+	}
 }
