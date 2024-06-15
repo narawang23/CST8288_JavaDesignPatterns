aa3c6e5e8ca6c4bb1d52a4adbac129146013deec
Nara Wang <nara.wang04@gmail.com>
2024-06-14 21:47:43 -0400

    database update

--- src/main/java/com/algonquin/cst8288/assignment2/database/DBOperations.java
+++ src/main/java/com/algonquin/cst8288/assignment2/database/DBOperations.java
@@ -21,17 +21,17 @@ public class DBOperations {
         this.connection = DriverManager.getConnection(url, username, password);
     }
 
-    public Event retrieveEvent(Event eventId) {
-
-        String sql = "SELECT * FROM events where event_id = ?";
+    public Event retrieveEvent(int eventId) {
+        Event event = null;
+        String sql = "SELECT * FROM events WHERE event_id = ?";
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setInt(1,
-                    event_id);
+            preparedStatement.setInt(1, eventId);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
                 event = new Event();
+                event.setEvent_id(resultSet.getInt("event_id"));
                 event.setEventName(resultSet.getString("eventName"));
                 event.setEventDescription(resultSet.getString("eventDescription"));
                 event.setEventActivities(resultSet.getString("eventActivities"));
@@ -43,4 +43,48 @@ public class DBOperations {
 
         return event;
     }
+
+    public void createEvent(Event event) {
+        String sql = "INSERT INTO events (event_name, event_description, event_activities, admission_fees) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, event.getEventName());
+            preparedStatement.setString(2, event.getEventDescription());
+            preparedStatement.setString(3, event.getEventActivities());
+            preparedStatement.setDouble(4, event.getAdmissionFees());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateEvent(Event event) {
+        String sql = "UPDATE events SET event_name = ?, event_description = ?, event_activities = ?, admission_fees = ? WHERE event_id = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, event.getEventName());
+            preparedStatement.setString(2, event.getEventDescription());
+            preparedStatement.setString(3, event.getEventActivities());
+            preparedStatement.setDouble(4, event.getAdmissionFees());
+            preparedStatement.setInt(5, event.getEvent_id());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteEvent(int eventId) {
+        String sql = "DELETE FROM events WHERE event_id = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, eventId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
